f462fa0a91377bd541a4bea0de661dd0e40b9b6f
Shanvi Priyadarshini <smarikadalei@gmail.com>
2026-07-17 13:56:30 +0530

    Updated product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('./product');
+const Product = require('../models/Product');
 const { protect, admin } = require('../middleware/auth');
 
 // Public: Get all products
